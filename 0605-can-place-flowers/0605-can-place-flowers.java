d06de8e66da56c16614079bf674726b33464d7a9
Jai Punjabi <126332747+Jai-Punjabi@users.noreply.github.com>
2024-07-19 12:32:58 +0530

    Update 0605-can-place-flowers.java

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -6,8 +6,6 @@ class Solution {
         
         int count = 0;
         for (int i = 0; i < flowerbed.length; i++) {
-            // the curr plot must be empty && the previous plot is empty && the next plot is empty
-            // exception: the first plot is empty || the last is empty
             if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0)
             && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
                 flowerbed[i] = 1;
